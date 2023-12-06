44fa908e459445719e2f2ca43e0c090dd3d993a7
leigh966 <leigh966@hotmail.com>
2023-12-06 14:56:59 +0000

    Fetching projects from the backend and passing them into a home page (which currently just logs the projects to console)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
+import { useEffect, useState } from "react";
+import Home from "./Home";
+
+async function getProjects(setProjects)
+{
+    try
+    {
+        const response = await fetch("http://127.0.0.1:3001/projects");
+        if(response.status==200)
+        {
+            const json = await response.json();
+            setProjects(json);
+        }
+        else
+        {
+            const text = await response.text();
+            console.log(text);
+        }
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+    
+
+}
+
 export default function App()
 {
-    return "Hello World";
+    const [projects, setProjects] = useState([]);
+    useEffect(()=>getProjects(setProjects),[]);
+    
+    return <Home projects={projects}/>
 }
---
+++ src/Home.js
@@ -0,0 +1,4 @@
+export default function Home({projects})
+{
+    console.log(projects);
+}
