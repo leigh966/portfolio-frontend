7d411aef63fd5825d195e5e3bfecd07e776705b0
leigh966 <leigh966@hotmail.com>
2023-12-07 10:14:19 +0000

    Added the last_updated field to the project tab element

--- src/ProjectTab.js
+++ src/ProjectTab.js
@@ -1,10 +1,12 @@
 import "./ProjectTab.css"
+
 export default function ProjectTab({data})
 {
     return(
         <div className="project-tab">
         <h3 className="project-tab-name">{data.name}</h3>
         <p className="project-tab-description">{data.description}</p>
+        <p className="project-tab-update">Last Updated: {data.last_updated.substring(0,10)}</p>
         </div>
     )
 }
