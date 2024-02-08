2d5c7616e85d1f3744e1631888b7cd3e67f9252c
Leigh966 <leigh966@hotmail.com>
2024-02-08 15:23:18 +0000

    added images and swapped descriptions for taglines

--- src/ProjectTab.js
+++ src/ProjectTab.js
@@ -1,12 +1,18 @@
-import "./ProjectTab.css"
+import "./ProjectTab.css";
+import { BACKEND_URL } from "./WebConfig";
 
-export default function ProjectTab({data})
-{
-    return(
-        <div className="project-tab">
-        <h3 className="project-tab-name">{data.name}</h3>
-        <p className="project-tab-description">{data.description}</p>
-        <p className="project-tab-update">Last Updated: {data.last_updated.substring(0,10)}</p>
-        </div>
-    )
+export default function ProjectTab({ data }) {
+  return (
+    <div className="project-tab">
+      <img
+        className="project-tab-img"
+        src={BACKEND_URL + "image/" + data.image_filename}
+      />
+      <h3 className="project-tab-name">{data.name}</h3>
+      <p className="project-tab-description">{data.tagline}</p>
+      <p className="project-tab-update">
+        Last Updated: {data.last_updated.substring(0, 10)}
+      </p>
+    </div>
+  );
 }
