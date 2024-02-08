3690b28787861f834c748ea770476377b4b81a27
Leigh966 <leigh966@hotmail.com>
2024-02-08 17:17:34 +0000

    resized banner images in project page to fill the top

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -1,10 +1,14 @@
 import ProjectBannerImage from "./ProjectBannerImage";
 import { BACKEND_URL } from "./WebConfig";
+import "./ProjectPage.css";
 
 export function ProjectPage({ project }) {
   return (
     <>
-      <ProjectBannerImage filename={project.image_filename} />
+      <ProjectBannerImage
+        filename={project.image_filename}
+        className={"project-page-banner-img"}
+      />
       <h1>{project.name}</h1>
       <h3>{project.tagline}</h3>
       <p>{project.description}</p>
