73c6aae0904747776e0dcab731ba66ab1e6417ff
Leigh966 <leigh966@hotmail.com>
2024-09-10 17:01:19 +0100

    massive aesthetic improvements to the project cards

--- src/Projects/ProjectTab.js
+++ src/Projects/ProjectTab.js
@@ -1,18 +1,31 @@
 import ProjectBannerImage from "../ProjectBannerImage";
 import "./ProjectTab.css";
+import { useState, useEffect } from "react";
+import { client } from "../client";
 
 export default function ProjectTab({ data, onClick }) {
+  const [imageUrl, setImageUrl] = useState(
+    "https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg"
+  );
+  useEffect(() => {
+    if (!data.image_filename) return;
+    client.getImageUrl(data.image_filename).then(setImageUrl);
+  }, []);
   return (
-    <div className="project-tab" onClick={onClick}>
-      <ProjectBannerImage
-        className="project-tab-img"
-        filename={data.image_filename}
-      />
-      <h3 className="project-tab-name">{data.name}</h3>
-      <p className="project-tab-description">{data.tagline}</p>
-      <p className="project-tab-update">
-        Last Updated: {data.last_updated.substring(0, 10)}
-      </p>
+    <div
+      className="project-tab"
+      onClick={onClick}
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    >
+      <div className="project-card-text-container">
+        <h3 className="project-tab-name">{data.name}</h3>
+        <p className="project-tab-description">{data.tagline}</p>
+        <p className="project-tab-update">
+          Last Updated: {data.last_updated.substring(0, 10)}
+        </p>
+      </div>
     </div>
   );
 }
